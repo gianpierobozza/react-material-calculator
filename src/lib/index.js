e58b209195da990e0ee4c255a1505db8743137e8
gianpierobozza <gianpiero.bozza@gmail.com>
2022-01-23 19:39:33 +0100

    prep for npm publish

---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+import ReactMuiCalculator from "./components/ReactMuiCalculator"
+import CalcKeypad from "./components/CalcKeypad"
+import CalcDisplay from "./components/CalcDisplay"
+import CalcButton from "./components/CalcButton"
+
+export { ReactMuiCalculator, CalcKeypad, CalcDisplay, CalcButton }
